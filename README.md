75ddaabe3f097c85aaa24407acd3e4d8154764f5
WalterZStark <walterzstark@gmail.com>
2023-07-09 23:01:55 -0400

    Added bottom view and front view camera viewing

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 # Code to test the basics of SLAM and CV object detection
 
 
+7-9-2023 - Second Push
+- Updated DJI Tello Firmware to latest version (2.05.01.19) on app to get down view capabilities 
+- Added down viewing capabilities
+
+
 7-9-2023
 - Make sure to turn on Airplane mode before flying
 - Recieved Error: "djitellopy.tello.TelloException: Command 'left 100' was unsuccessful for 4 tries. Latest response:      'error No valid imu'"
@@ -11,3 +16,4 @@
     - Used inspiration from: https://github.com/damiafuentes/DJITelloPy/blob/master/examples/record-video.py
 - Issues with Camera Feed being Blue
     - Solved with "im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)"
+
--- test-takeoff.py
+++ test-takeoff.py
@@ -1,29 +1,6 @@
 #Test-takeoff.py
+# Written by Walter Stark
 
-
-
-
-'''
-from djitellopy import Tello
-
-tello = Tello()
-
-
-
-tello.connect()
-tello.takeoff()
-tello.get_current_state()
-tello.move_left(100)
-tello.rotate_counter_clockwise(90)
-tello.move_forward(100)
-
-
-# Command to get downvision camera
-tello.send_command_with_return("downvision 1")
-
-
-tello.land()
-'''
 
 
 import time, cv2
@@ -32,34 +9,45 @@ from djitellopy import Tello
 
 tello = Tello()
 
+# Connect to Tello
 tello.connect()
 
+
 keepRecording = True
+# Start Stream
 tello.streamon()
+
 print(tello.get_battery())
+# Turn on down vision
+tello.send_command_with_return("downvision 1")
+downvision = True
 
 
-
-#rame_read = tello.get_frame_read()
-
-
-
-# Imae output
+# Create a function to output images
 def imgOut():
-    # Create a function to output images
-    #height, width, _ = frame_read.frame.shape
-    #video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
-
+ 
+    
+    # Check to see if recording has finished
     while keepRecording:
-        singular_frame = tello.get_frame_read().frame
-        img = cv2.resize(singular_frame, (360, 240))
-        im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cv2.imshow('img', im_rgb)
-        cv2.waitKey(1)
+        # Try Catch added in case no frame when looking downward
+        #try:
+            if(downvision):
+                singular_frame = tello.get_frame_read().frame
+                cropped_img = singular_frame[0:240, 0:320]
+                cv2.imshow('img', cropped_img)
+                cv2.waitKey(1)
+            else: 
+                singular_frame = tello.get_frame_read().frame
+                img = cv2.resize(singular_frame, (360, 240))
+                im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                cv2.imshow('img', im_rgb)
+                cv2.waitKey(1)
+        #except:
+            #print("no frame")
 
     
 
-
+# Create a thread to run image viewing in the background
 recorder = Thread(target=imgOut)
 recorder.start()
 
